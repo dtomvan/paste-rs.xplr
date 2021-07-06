4ea6099a5485a870f8fca1ce922337e8bee5295b
Tom van Dijk <18gatenmaker6@gmail.com>
2021-07-06 18:47:09 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-xplr plugin template
-====================
+# paste.rs.xplr
+Use this plugin to paste your files to [paste.rs](https://paste.rs/), and open/delete them later in fzf.
 
-Use this template to [write your own xplr plugin](https://arijitbasu.in/xplr/en/writing-plugins.html).
+## Credit
+I don't take any credit for this plugin, I ran [@sayanarijit](https://github.com/sayanarijit)'s yml-to-lua converter over his own xplr hack. 
+
+## Requirements
+
+- [fzf](https://github.com/junegunn/fzf)
+- [sd](https://github.com/chmln/sd)
+- [curl](https://github.com/curl/curl)
 
 
-Requirements
-------------
-
-- Some tool
-
-
-Installation
-------------
-
-### Install manually
+## Installation
 
 - Add the following line in `~/.config/xplr/init.lua`
 
@@ -26,26 +24,27 @@ Installation
   ```bash
   mkdir -p ~/.config/xplr/plugins
 
-  git clone https://github.com/me/{plugin}.xplr ~/.config/xplr/plugins/{plugin}
+  git clone https://github.com/dtomvan/paste.rs.xplr ~/.config/xplr/plugins/paste-rs
   ```
 
 - Require the module in `~/.config/xplr/init.lua`
 
   ```lua
-  require("{plugin}").setup()
+  require("paste-rs").setup()
   
   -- Or
   
-  require("{plugin}").setup{
-    mode = "action",
-    key = ":",
+  require("paste-rs").setup{
+    mode = "default",
+    key = "P",
   }
 
-  -- Type `::` and enjoy.
+  -- Type `Pp` and enjoy.
   ```
 
 
-Features
---------
+## Features
 
-- Some cool feature
+- Paste your files directly to paste.rs
+- List all of your uploads (from the current session) and open them later.
+- Delete your uploads (from the current session)
--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,98 @@
 local function setup(args)
-  local xplr = xplr
-  -- do stuff with xplr
+    local xplr = xplr
+    xplr.config.modes.custom["paste.rs"] = {
+        name = "paste.rs",
+        key_bindings = {
+            on_key = {
+                p = {
+                    help = "paste",
+                    messages = {
+                        {
+                            BashExec = [===[
+                            PTH=$(basename "${XPLR_FOCUS_PATH:?}")
+                            DEST="${XPLR_SESSION_PATH:?}/paste.rs.list"
+                            curl --data-binary "@${PTH:?}" "https://paste.rs" | tee -a "${DEST:?}"
+                            echo
+                            read -p "[enter to continue]"
+                            ]===],
+                        },
+                        "PopMode",
+                    },
+                },
+                l = {
+                    help = "list",
+                    messages = {
+                        {
+                            BashExec = [===[
+                            cat "${XPLR_SESSION_PATH:?}/paste.rs.list"
+                            echo
+                            read -p "[enter to continue]"
+                            ]===],
+                        },
+                        "PopMode",
+                    },
+                },
+                o = {
+                    help = "search and open",
+                    messages = {
+                        {
+                            BashExec = [===[
+                            DEST="${XPLR_SESSION_PATH:?}/paste.rs.list"
+                            URL=$(fzf --preview "curl -s '{}'" < "${DEST:?}")
+                            if [ "$URL" ]; then
+                                OPENER=$(which xdg-open)
+                                ${OPENER:-open} "${URL:?}"
+                            fi
+                            ]===],
+                            },
+                            "PopMode",
+                        },
+                    },
+                    d = {
+                        help = "search and delete",
+                        messages = {
+                            {
+                                BashExec = [===[
+                                DEST="${XPLR_SESSION_PATH:?}/paste.rs.list"
+                                URL=$(fzf --preview "curl -s '{}'" < "${DEST:?}")
+                                if [ "$URL" ]; then
+                                    curl -X DELETE "${URL:?}"
+                                    sd "${URL:?}n" "" "${DEST:?}"
+                                    echo
+                                    read -p "[enter to continue]"
+                                fi
+                                ]===],
+                            },
+                                "PopMode",
+                            },
+                        },
+                        esc = {
+                            help = "cancel",
+                            messages = {
+                                "PopMode",
+                            }
+                        }
+                    }
+                }
+    }
+    if args == nil then
+        args = {}
+    end
+    if args.mode == nil then
+        args.mode = "default"
+    end
+    if args.key == nil then
+        args.key = "P"
+    end
+    xplr.config.modes.builtin[args.mode].key_bindings.on_key[args.key] = {
+        help = "paste.rs",
+        messages = {
+            "PopMode",
+            {
+                SwitchModeCustom = "paste.rs"
+            },
+        }
+    }
 end
 
 return { setup = setup }
